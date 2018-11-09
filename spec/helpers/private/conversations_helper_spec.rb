76e2a30a39e241970b0d6cdf0abafe07b8648429
Haruki Miyagi <haruki.miyagi1112@gmail.com>
2018-11-10 01:09:21 +0900

    Update テストコード rspec で落ちた個所

--- spec/helpers/private/conversations_helper_spec.rb
+++ spec/helpers/private/conversations_helper_spec.rb
@@ -16,13 +16,11 @@ RSpec.describe Private::ConversationsHelper, type: :helper do
 
     it "returns load_messages partial's path" do
       create(:private_message, conversation_id: conversation.id)
-      expect(helper.load_private_messages(conversation)).to eq
-      'private/conversations/conversation/messages_list/link_to_previous_messages'
+      expect(helper.load_private_messages(conversation)).to eq('private/conversations/conversation/messages_list/link_to_previous_messages')
     end
 
     it "returns empty partial's path" do
-      expect(helper.load_private_messages(conversation)).to eq
-      'shared/empty_partial'
+      expect(helper.load_private_messages(conversation)).to eq('shared/empty_partial')
     end
   end
 end
