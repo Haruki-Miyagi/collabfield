cf2304178aafdeadf4a212f2ea78b262298a6adf
Haruki Miyagi <haruki.miyagi1112@gmail.com>
2018-11-11 21:13:57 +0900

    Add 会話フォームに連絡先追加のアイコンを表示する

--- spec/helpers/private/conversations_helper_spec.rb
+++ spec/helpers/private/conversations_helper_spec.rb
@@ -23,4 +23,71 @@ RSpec.describe Private::ConversationsHelper, type: :helper do
       expect(helper.load_private_messages(conversation)).to eq('shared/empty_partial')
     end
   end
+
+  context '#add_to_contacts_partial_path' do
+    let(:contact) { create(:contact) }
+
+    it "returns an empty partial's path" do
+      allow(helper).to receive(:recipient_is_contact?).and_return(true)
+      expect(helper.add_to_contacts_partial_path(contact)).to eq(
+        'shared/empty_partial'
+      )
+    end
+
+    it "returns add_user_to_contacts partial's path" do
+      allow(helper).to receive(:recipient_is_contact?).and_return(false)
+      allow(helper).to receive(:unaccepted_contact_exists).and_return(false)
+      expect(helper.add_to_contacts_partial_path(contact)).to eq(
+        'private/conversations/conversation/heading/add_user_to_contacts'
+      )
+    end
+  end
+
+  context 'private scope' do
+    let(:current_user) { create(:user) }
+    let(:recipient) { create(:user) }
+
+    context '#unaccepted_contact_exists' do
+      it 'returns false' do
+        contact = create(:contact, accepted: true)
+        expect(helper.instance_eval do
+          unaccepted_contact_exists(contact)
+        end).to eq false
+      end
+
+      it 'returns false' do
+        contact = nil
+        expect(helper.instance_eval do
+          unaccepted_contact_exists(contact)
+        end).to eq false
+      end
+
+      it 'returns true' do
+        contact = create(:contact, accepted: false)
+        expect(helper.instance_eval do
+          unaccepted_contact_exists(contact)
+        end).to eq true
+      end
+    end
+
+    context '#recipient_is_contact?' do
+      it 'returns false' do
+        allow(helper).to receive(:current_user).and_return(current_user)
+        assign(:recipient, recipient)
+        create_list(:contact, 2, user_id: current_user.id, accepted: true)
+        expect(helper.instance_eval { recipient_is_contact? }).to eq false
+      end
+
+      it 'returns true' do
+        allow(helper).to receive(:current_user).and_return(current_user)
+        assign(:recipient, recipient)
+        create_list(:contact, 2, user_id: current_user.id, accepted: true)
+        create(:contact,
+               user_id: current_user.id,
+               contact_id: recipient.id,
+               accepted: true)
+        expect(helper.instance_eval { recipient_is_contact? }).to eq true
+      end
+    end
+  end
 end
