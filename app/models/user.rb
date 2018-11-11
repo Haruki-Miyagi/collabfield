5e177fb7aaa7d5948bfef32c96c8adbd0095606e
Haruki Miyagi <haruki.miyagi1112@gmail.com>
2018-11-11 19:52:56 +0900

    Add 関連付けとヘルパーメソッドをUserモデルに追加する

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,36 @@ class User < ApplicationRecord
            foreign_key: :sender_id,
            class_name: 'Private::Conversation',
            dependent: :destroy
+
+  has_many :contacts, dependent: :destroy
+  has_many :all_received_contact_requests, # rubocop:disable Rails/HasManyOrHasOneDependent, Rails/InverseOf
+           class_name: 'Contact',
+           foreign_key: 'contact_id'
+  has_many :accepted_sent_contact_requests, -> { where(contacts: { accepted: true }) },
+           through: :contacts,
+           source: :contact
+  has_many :accepted_received_contact_requests, -> { where(contacts: { accepted: true }) },
+           through: :all_received_contact_requests,
+           source: :user
+  has_many :pending_sent_contact_requests, ->  { where(contacts: { accepted: false }) },
+           through: :contacts,
+           source: :contact
+  has_many :pending_received_contact_requests, -> { where(contacts: { accepted: false }) },
+           through: :all_received_contact_requests,
+           source: :user
+
+  # すべての連絡先の取得
+  def all_active_contacts
+    accepted_sent_contact_requests | accepted_received_contact_requests
+  end
+
+  # 保留中の送受信された連絡先を取得する
+  def pending_contacts
+    pending_sent_contact_requests | pending_received_contact_requests
+  end
+
+  # 連絡先レコードを取得する
+  def contact(contact)
+    Contact.where(user_id: id, contact_id: contact.id)[0]
+  end
 end
