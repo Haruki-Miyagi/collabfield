3f0efe0233674a04eb688b0134284ef8a6771039
Haruki Miyagi <haruki.miyagi1112@gmail.com>
2018-11-11 21:47:40 +0900

    非公開会話のウィンドウにボタンを追加する

--- spec/helpers/private/conversations_helper_spec.rb
+++ spec/helpers/private/conversations_helper_spec.rb
@@ -89,5 +89,98 @@ RSpec.describe Private::ConversationsHelper, type: :helper do
         expect(helper.instance_eval { recipient_is_contact? }).to eq true
       end
     end
+
+    context '#conv_heading_class' do
+      let(:contact) { create(:contact) }
+
+      it 'returns a conversation-heading-full class' do
+        contact.update(accepted: false)
+        expect(helper.conv_heading_class(contact)).to eq 'conversation-heading-full'
+      end
+
+      it 'returns a conversation-heading class' do
+        contact.update(accepted: true)
+        expect(helper.conv_heading_class(contact)).to eq 'conversation-heading'
+      end
+    end
+
+    context '#get_contact_record' do
+      it 'returns a Contact record' do
+        contact = create(:contact, user_id: current_user.id, contact_id: recipient.id)
+        allow(helper).to receive(:current_user).and_return(current_user)
+        expect(helper.get_contact_record(recipient)).to eq contact
+      end
+    end
+
+    context '#unaccepted_contact_request_partial_path' do
+      let(:contact) { contact = create(:contact) }
+
+      it "returns sent_by_current_user partial's path" do
+        allow(helper).to receive(:unaccepted_contact_exists).and_return(true)
+        allow(helper).to receive(:request_sent_by_user).and_return(true)
+        expect(helper.unaccepted_contact_request_partial_path(contact)).to eq(
+          'private/conversations/conversation/request_status/sent_by_current_user' 
+        )
+      end
+
+      it "returns sent_by_recipient partial's path" do
+        allow(helper).to receive(:unaccepted_contact_exists).and_return(true)
+        allow(helper).to receive(:request_sent_by_user).and_return(false)
+        expect(helper.unaccepted_contact_request_partial_path(contact)).to eq(
+          'private/conversations/conversation/request_status/sent_by_recipient'
+        )
+      end
+
+      it "returns an empty partial's path" do
+        allow(helper).to receive(:unaccepted_contact_exists).and_return(false)
+        expect(helper.unaccepted_contact_request_partial_path(contact)).to eq(
+          'shared/empty_partial'
+        )
+      end
+    end
+
+    context '#not_contact_no_request' do
+      let(:contact) { contact = create(:contact) }
+
+      it "returns send_request partial's path" do
+        allow(helper).to receive(:recipient_is_contact?).and_return(false)
+        allow(helper).to receive(:unaccepted_contact_exists).and_return(false)
+        expect(helper.not_contact_no_request_partial_path(contact)).to eq(
+          'private/conversations/conversation/request_status/send_request'
+        )
+      end
+
+      it "returns an empty partial's path" do
+        allow(helper).to receive(:recipient_is_contact?).and_return(true)
+        allow(helper).to receive(:unaccepted_contact_exists).and_return(false)
+        expect(helper.not_contact_no_request_partial_path(contact)).to eq(
+          'shared/empty_partial'
+        )
+      end
+
+      it "returns an empty partial's path" do
+        allow(helper).to receive(:recipient_is_contact?).and_return(false)
+        allow(helper).to receive(:unaccepted_contact_exists).and_return(true)
+        expect(helper.not_contact_no_request_partial_path(contact)).to eq(
+          'shared/empty_partial'
+        )
+      end
+    end
+
+    context 'private scope' do
+      context '#request_sent_by_user' do
+        it 'returns true' do
+          contact = create(:contact, user_id: current_user.id)
+          allow(helper).to receive(:current_user).and_return(current_user)
+          expect(helper.instance_eval { request_sent_by_user(contact) }).to eq true
+        end
+
+        it 'returns false' do
+          contact = create(:contact, user_id: recipient.id)
+          allow(helper).to receive(:current_user).and_return(current_user)
+          expect(helper.instance_eval { request_sent_by_user(contact) }).to eq false
+        end
+      end
+    end
   end
 end
