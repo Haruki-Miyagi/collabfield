f6a8988985d9235ad21d3efacb7053b19f360c64
Haruki Miyagi <haruki.miyagi1112@gmail.com>
2018-11-13 18:11:12 +0900

    - グループ/会話内に_conversation.html.erbを作成する - 定義するadd_people_to_group_conv_list and write specs for it

--- app/helpers/group/conversations_helper.rb
+++ app/helpers/group/conversations_helper.rb
@@ -1,2 +1,12 @@
 module Group::ConversationsHelper
+  def add_people_to_group_conv_list(conversation)
+    contacts = current_user.all_active_contacts
+    users_in_conv = conversation.users
+    add_people_to_conv_list = []
+    contacts.each do |contact|
+      # if the contact is already in the conversation, remove it from the list
+      add_people_to_conv_list << contact unless users_in_conv.include?(contact)
+    end
+    add_people_to_conv_list
+  end
 end
