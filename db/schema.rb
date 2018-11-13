6332213de20fffe63f70894309f8c7eee796a5b6
Haruki Miyagi <haruki.miyagi1112@gmail.com>
2018-11-13 14:38:21 +0900

    Add group_conversations_usersテーブルの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_12_131137) do
+ActiveRecord::Schema.define(version: 2018_11_13_053306) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,13 @@ ActiveRecord::Schema.define(version: 2018_11_12_131137) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "group_conversations_users", id: false, force: :cascade do |t|
+    t.integer "conversation_id"
+    t.integer "user_id"
+    t.index ["conversation_id"], name: "index_group_conversations_users_on_conversation_id"
+    t.index ["user_id"], name: "index_group_conversations_users_on_user_id"
   end
 
   create_table "group_messages", force: :cascade do |t|
