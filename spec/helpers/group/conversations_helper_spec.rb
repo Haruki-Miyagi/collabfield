7658add00a9e4f055e7b52d201cdf7e4140e9b8d
Haruki Miyagi <haruki.miyagi1112@gmail.com>
2018-11-13 18:30:19 +0900

    グループ会話のウィンドウもレンダリングできるようにする

--- spec/helpers/group/conversations_helper_spec.rb
+++ spec/helpers/group/conversations_helper_spec.rb
@@ -10,7 +10,7 @@ require 'rails_helper'
 #     end
 #   end
 # end
-RSpec.describe Group::ConversationsHelper, type: :helper do
+RSpec.describe Group::ConversationsHelper, type: :helper do # rubocop:disable Metrics/BlockLength
   context '#add_people_to_group_conv_list' do
     let(:conversation) { create(:group_conversation) }
     let(:current_user) { create(:user) }
@@ -33,6 +33,22 @@ RSpec.describe Group::ConversationsHelper, type: :helper do
       conversation.users << current_user
       allow(helper).to receive(:current_user).and_return(current_user)
       expect(helper.add_people_to_group_conv_list(conversation)).to include user
+    end
+  end
+
+  context '#load_group_messages_partial_path' do
+    let(:conversation) { create(:group_conversation) }
+    it "returns load_messages partial's path" do
+      create_list(:group_message, 2, conversation_id: conversation.id)
+      expect(helper.load_group_messages_partial_path(conversation)).to eq(
+        'group/conversations/conversation/messages_list/link_to_previous_messages'
+      )
+    end
+
+    it "returns an empty partial's path" do
+      expect(helper.load_group_messages_partial_path(conversation)).to eq(
+        'shared/empty_partial'
+      )
     end
   end
 end
