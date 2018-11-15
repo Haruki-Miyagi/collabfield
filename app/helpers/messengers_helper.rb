15517ea9ba8de1710b5b79017b2af72d3f6a3368
Haruki Miyagi <haruki.miyagi1112@gmail.com>
2018-11-15 22:42:38 +0900

    MessengersControllerのインデックスアクションのテンプレートを作成する

--- app/helpers/messengers_helper.rb
+++ app/helpers/messengers_helper.rb
@@ -1,2 +1,11 @@
 module MessengersHelper
+  module MessengersHelper
+    def conversations_list_item_partial_path(conversation)
+      if conversation.class == Private::Conversation
+        'messengers/index/conversations_list_item/private'
+      else
+        'messengers/index/conversations_list_item/group'
+      end
+    end
+  end
 end
