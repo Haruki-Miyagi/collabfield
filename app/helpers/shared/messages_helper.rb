123a651d6d1942eaf76170e7c8aebd7b4bf5c52d
Haruki Miyagi <haruki.miyagi1112@gmail.com>
2018-11-15 22:58:05 +0900

    Shared :: MessagesHelperにautoload_messenger_messagesを定義する

--- app/helpers/shared/messages_helper.rb
+++ app/helpers/shared/messages_helper.rb
@@ -14,4 +14,19 @@ module Shared::MessagesHelper
       'shared/empty_partial'
     end
   end
+
+  # スクロールバーが表示されるまでメッセンジャーに前のメッセージをロードする
+  def autoload_messenger_messages
+    if @is_messenger == 'true'
+      # 以前のメッセージが存在する場合は、それらをロードします
+      if @messages_to_display_offset != 0
+        'shared/load_more_messages/messenger/load_previous_messages'
+      else
+        # 削除負荷前のメッセージのリンク
+        'shared/load_more_messages/messenger/remove_previous_messages_link'
+      end
+    else
+      'shared/empty_partial'
+    end
+  end
 end
