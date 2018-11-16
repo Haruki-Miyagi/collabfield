b9b69cadd4172f421f291286f5b58207e86fbf8b
Haruki Miyagi <haruki.miyagi1112@gmail.com>
2018-11-16 22:12:48 +0900

    Add render Mobile backlink

--- app/assets/javascripts/messenger.js
+++ app/assets/javascripts/messenger.js
@@ -7,4 +7,19 @@ $(document).on('turbolinks:load ajax:complete', function() {
         $('.load-more-messages', this)[0].click();
         $('.conversation .messages-list .loading-more-messages').hide();
     }
+
+    // メッセンジャーが小さいスクリーンデバイスで開かれている場合
+    // モバイルデバイスのメッセンジャーのバージョンを表示する
+    $(".messenger .col-sm-2 ul").on( "click", "a", function() {
+        var col_2_width = $('.messenger .col-sm-2').css('width');
+        var window_width = '' + $('.messenger').width() + 'px';
+        // ブートストラップカラムがスタックされているかどうかを確認します（小さいデバイスでページを開く）
+        if (col_2_width == window_width) {
+            $('body nav').hide();
+            $('.messenger .col-sm-2').hide();
+            $('.messenger .col-sm-10').show();
+            $('body').css('margin', '0 0 0 0');
+            $('.messenger').css('height', '100vh');
+        }
+    });
 });
